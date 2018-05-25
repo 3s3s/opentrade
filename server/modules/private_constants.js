f5b7f91c1ab98a4dbc5da5cea0d9c2e2fe3f036c
karthik <kartx.rockeveryone@gmail.com>
2018-05-25 16:36:52 +0530

    Create private_constants.js

---
+++ server/modules/private_constants.js
@@ -0,0 +1,12 @@
+'use strict';
+
+exports.recaptcha_priv_key = 'YOUR_GOOGLE_RECAPTCHA_PRIVATE_KEY';
+exports.password_private_suffix = 'LONG_RANDOM_STRING1';
+exports.SSL_KEY = '../ssl_certificates/privkey.pem'; //change to your ssl certificates private key
+exports.SSL_CERT = '../ssl_certificates/fullchain.pem'; //change to your ssl certificates fullchain
+
+exports.walletspassphrase = {
+    'MC' : 'LONG_RANDOM_STRING2',
+    'BTC' : 'LONG_RANDOM_STRING3',
+    'DOGE' : 'LONG_RANDOM_STRING4'
+};
